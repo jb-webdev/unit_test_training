e317fb47ac7554c1a94de186b67536be4b472852
j.boero <boero.dw@gmail.com>
2021-04-07 08:06:29 +0200

    first commit

---
+++ calcuHelper.js
@@ -0,0 +1,12 @@
+function calculAir (longeur, largeur) {
+    return multiplication(longeur, largeur);
+}
+
+function calculAirCarre (longeur) {
+    return calculAir(longeur, longeur);
+}
+
+function multiplication(a, b) {
+    return a*b;
+}
+
