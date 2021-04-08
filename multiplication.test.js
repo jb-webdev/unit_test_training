ce5a5fb70460ca7f054bef5c1e9b1b38b5526cc5
j.boero <boero.dw@gmail.com>
2021-04-08 11:03:50 +0200

    premier test avec jest réussi

--- multiplication.test.js
+++ multiplication.test.js
@@ -2,3 +2,9 @@
 // etape 2 : redaction du test
 // etape 3 : assertion
 
+
+const multiplication = require ( './multiplication');
+
+test('10 multiplié par 9 = 90', () => {
+    expect(multiplication(9,10)).toBe(90)
+})
